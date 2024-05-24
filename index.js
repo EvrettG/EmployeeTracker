cb65328678f3c7423b477acc06b6ac3d4d51c057
EvrettG <skyrift@tpg.com.au>
2024-05-24 18:38:53 +1000

    Corrected index issue

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // The index.js in the main directory acts as a client and runs the inquirer commands and sends api calls
+const inquirer = require('inquirer');
+const colors = require('colors');
 
+const questions=[
+    {
+        
+    }
+]
+
+function init(){
+    inquirer.prompt(questions).then((data)=>{
+        // write functions here
+    })
+}
+
+init();
 
 
 
