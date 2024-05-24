8c5570d7b43c6fff0f0b9ab3385bc0086f88d58a
EvrettG <skyrift@tpg.com.au>
2024-05-24 20:24:57 +1000

    Issue with Insomnia testing fixed

--- Routes/departments.js
+++ Routes/departments.js
@@ -4,12 +4,14 @@ const pool = require('../utils/connection');
 
 departments.get('/', (req,res)=>{
     
-    pool.query('SELECT * FROM departments', (err, res) => {
+    pool.query('SELECT * FROM departments', (err, res1) => {
         if (err) {
           console.error('Error executing query', err.stack);
         } else {
           console.log('Query result:');
-          console.table(res.rows)
+          console.table(res1.rows);
+        //   Note following code is for insomina to stop timeouts
+          res.json("Reults returned succefully");
         }
         pool.end(); // Close the pool when done
       });
